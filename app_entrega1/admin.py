ac1d722ec94e66239ddb2fa0ef4450d5677eb60b
Andrea Vazquez <andreaceciliavazquez@yahoo.com.ar>
2022-04-25 01:12:14 -0300

    prueba sql

---
+++ app_entrega1/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from app_entrega1.models import Libros
+
+# Register your models here.
+admin.site.register(Libros)
---
+++ app_login/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from app_login.models import Avatar
+
+# Register your models here.
+admin.site.register(Avatar)
